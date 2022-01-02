714d6c2982525fea6565ddaec8380ec83a17169b
ManavSah2002 <manavminsk@gmail.com>
2022-01-02 17:38:24 +0530

    commit on 'master'

--- Hello/Home/urls.py
+++ Hello/Home/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from Home import views
 urlpatterns = [
-    path("",views.index,name='Home')
+    path("", views.index, name='Home'),
+    path("contact", views.contact, name='contact'),
+    path("services", views.services, name='services'),
+    path("about", views.about, name='about'),
 ]
--- Hello/Home/views.py
+++ Hello/Home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render,HttpResponse
 def index (request):
     return render(request,'index.html')
     #return HttpResponse("this is home page")
+def about (request):
+    return HttpResponse("This is about page")
+
+def services (request):
+    return HttpResponse("This is service page")
+
+def contact (request):
+    return HttpResponse("This is contact page")
+
